6131026a61e8beb4f4867540c688fd381bd9eef3
Ash <aishwarya>
2024-09-26 21:58:42 +0530

    update the output for emotion_detection.py

--- venv/emotion_detection.py
+++ venv/emotion_detection.py
@@ -1,19 +1,23 @@
 import requests
-import json
 
 
-def emotion_detector(emotion):
-    """ URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    Input json: { "raw_document": { "text": text_to_analyse } }"""
-    url = "http://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
-    headers = {'grpc-metadata-mm-model-id': 'emotion_aggregated-workflow_lang_en_stock'}
-    body = {"raw_document": {"text": emotion}}
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
 
-    response = requests.post(url, json=body, headers=headers)
-    print("hi hi ")
-    text = ''
-    return text
+    response = requests.post(url, json=input_json, headers=headers)
+    print(response.json().get('emotionprediction', {}).get('emotionmentions', {}).get('text', ''))
+    l = response.json().get('emotionPredictions', None);
+    emotion_mention = l[0]['emotionMentions'][0]['span']['text']
 
-
-emotion_detector("I love you ! ")
+    if response.status_code == 200:
+        return emotion_mention
+    else:
+        raise Exception(f"Error: {response.status_code}, {response.text}")
