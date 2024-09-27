7b1fb32ca55f73710f0f321dd2a96b43225a77b6
areez <areez.bd@gmail.com>
2024-09-27 08:57:11 -0400

    Task-8-a-b-complete

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+This is the starting file of the server.
+It serves api request with appropriate response
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +9,10 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """
+    It takes input text from user and send to appropriate function, 
+    then return response based on user text emotions. 
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     anger = response['anger']
@@ -24,6 +32,9 @@ def emo_detector():
 
 @app.route("/")
 def render_index_page():
+    """
+    It starts server on defined server and port for development. 
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
