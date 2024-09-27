bdbc74545b333eb47b0c61119a6f83cd89c060fd
areez <areez.bd@gmail.com>
2024-09-27 08:51:13 -0400

    Task-7-a-b-c-complete

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -11,17 +11,26 @@ def emotion_detector(text_to_analyse):
     formatted_response = json.loads(response.text)
 
     # Extract the required emotions
-    anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
-    disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
-    fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
-    joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
-    sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+    if response.status_code == 200:        
+        anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+        disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+        fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+        joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+        sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
     
     # Find the dominant emotion
-    emotion_list = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
-    dominant_emotion_index = emotion_list.index(max(emotion_list))
-    emotion_keys = ["anger", "disgust", "fear", "joy", "sadness"]
-    dominant_emotion_key = emotion_keys[dominant_emotion_index]
+        emotion_list = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+        dominant_emotion_index = emotion_list.index(max(emotion_list))
+        emotion_keys = ["anger", "disgust", "fear", "joy", "sadness"]
+        dominant_emotion_key = emotion_keys[dominant_emotion_index]
+    
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion_key = None
     
     # Return the formatted output
     result = {
