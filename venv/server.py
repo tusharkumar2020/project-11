11fd721c0deb91528444452a395ded89fc8ac221
Ash <aishwarya>
2024-09-28 22:24:20 +0530

    Added error handling function

--- venv/server.py
+++ venv/server.py
@@ -1,23 +1,41 @@
-from flask import Flask, jsonify
-from flask import make_response
-from flask import request
-from EmotionDetection.emotion_detection import emotion_detector
+"""
+A simple Flask application for emotion detection from text input.
+"""
 
+from flask import Flask, jsonify, request
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def get_emotion_detected():
+    """
+    This function returns the dominant emotion detected in the input text.
+    """
     text_to_analyze = request.args.get("q")
+
+    if not text_to_analyze:
+        # Return a 400 status code and a dictionary with None values
+        output = jsonify({
+            key: None for key in [
+                'happy', 'sad', 'angry', 'surprised', 'neutral', 'dominant_emotion'
+            ]
+        })
+        return output, 400
+
     emotion_results = emotion_detector(text_to_analyze)
 
-    # Create the response string
     emotions_str = ', '.join(
-        [f"'{key}': {value}" for key, value in emotion_results.items() if key != 'dominant_emotion']
+        [f"'{key}': {value}" for key, value in emotion_results.items()
+         if key != 'dominant_emotion']
     )
-    dominant_emotion = emotion_results['dominant_emotion']
 
-    # Format the output
+    # Create the response string
+    dominant_emotion = emotion_results.get('dominant_emotion')
+
+    if dominant_emotion is None:
+        return jsonify({"message": "Invalid text! Please try again!"})
+
     response_message = (
         f"For the given statement, the system response is {emotions_str}. "
         f"The dominant emotion is <strong>{dominant_emotion}</strong>."
@@ -25,5 +43,5 @@ def get_emotion_detected():
 
     return jsonify({"message": response_message})
 
-
-app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
