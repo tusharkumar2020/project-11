2b6c565b78c83c4a96b6b44bda67cfe0d599995d
SATHURJAN J <107549768+j-sathurjan@users.noreply.github.com>
2024-10-12 17:11:19 +0530

    emotion_detector has been modified to return a json output containing only the emotions and related scores of them and dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +7,21 @@ def emotion_detector(text_to_analyze):
     payload =  { "raw_document": { "text": text_to_analyze } }
     
     response = requests.post(url , json = payload , headers = header)
-    
-    return response.text
+    formatted_response = json.loads(response.text)
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    emotion_scores = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+    }
+    dominant_emotion_score = max(emotion_scores.values())
+    dominant_emotion_list = [dominant for dominant in emotion_scores if emotion_scores[dominant] == dominant_emotion_score]
+    output_json = emotion_scores.copy()
+    output_json.update({'dominant_emotion':dominant_emotion_list[0]})
+    return output_json
