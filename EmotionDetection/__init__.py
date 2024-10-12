0a207a8c9c81574c041144b8be482e38f0fd5947
SATHURJAN J <107549768+j-sathurjan@users.noreply.github.com>
2024-10-12 18:45:24 +0530

    emotion_detection packaged as EmotionDetection to keep the project organized

---
+++ EmotionDetection/__init__.py
@@ -0,0 +1,3 @@
+"""This init module is to expose the modules in this package
+"""
+from . import emotion_detection
--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,12 +1,24 @@
-import requests
+"""This module has function related to emotions detection
+"""
 import json
+import requests
 
 def emotion_detector(text_to_analyze):
+    """This function is for detect the emotion based on the text given.
+    it takes the string text and send a post request to watson api and get the text response
+    then based on the response convert it to json and then return required parameters as json
+
+    Args:
+        text_to_analyze (String): this is the text it takes to detect the emotion.
+
+    Returns:
+        dict: output_json which is dictionary out put contains required parameters.
+    """
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     payload =  { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url , json = payload , headers = header, timeout=10)
     
-    response = requests.post(url , json = payload , headers = header)
     formatted_response = json.loads(response.text)
     anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
     disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
@@ -24,4 +36,5 @@ def emotion_detector(text_to_analyze):
     dominant_emotion_list = [dominant for dominant in emotion_scores if emotion_scores[dominant] == dominant_emotion_score]
     output_json = emotion_scores.copy()
     output_json.update({'dominant_emotion':dominant_emotion_list[0]})
+    
     return output_json
