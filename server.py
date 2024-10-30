28e3b992dd028e23e247521207ebd510f00cce40
MDL <leon.ari00@hotmail.com>
2024-10-30 16:22:08 -0600

    solving bugs

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import emotion_detector
+from Emotion_Detection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
@@ -7,7 +7,7 @@ app = Flask("Emotion Detector")
 def emotion_detector_function():
     ''' This function calls the application
     '''
-    test_to_analyze = request.args.get('textToAnalyze')
+    text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
     if response['dominant_emotion'] is None:
@@ -21,7 +21,7 @@ def emotion_detector_function():
 
     return response_text
 
-@app.route("\")
+@app.route("/") 
 def render_index_page():
     ''' This is the function to render the html interface
     '''
