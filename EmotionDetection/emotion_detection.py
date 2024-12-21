6663f18649af1c41eac678594a8593ced7833a90
Carlitroxeno <carloscord@gmail.com>
2024-12-21 10:37:23 -0500

    Control de Errores web

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,20 +5,32 @@ def emotion_detector(text_to_analyse):  # Definir una función llamada emotion_d
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)  # Enviar una solicitud POST a la API con el texto y los encabezados
     
-    response_dict = response.json()
-    predictions = response_dict.get("emotionPredictions", [{}])[0]
-    emotions = predictions.get("emotion", {})
-    scores = {
-        'anger': emotions.get('anger', 0),
-        'disgust': emotions.get('disgust', 0),
-        'fear': emotions.get('fear', 0),
-        'joy': emotions.get('joy', 0),
-        'sadness': emotions.get('sadness', 0),
-    }
+    if response.status_code == 200:
+        response_dict = response.json()
+        predictions = response_dict.get("emotionPredictions", [{}])[0]
+        emotions = predictions.get("emotion", {})
+        scores = {
+            'anger': emotions.get('anger', 0),
+            'disgust': emotions.get('disgust', 0),
+            'fear': emotions.get('fear', 0),
+            'joy': emotions.get('joy', 0),
+            'sadness': emotions.get('sadness', 0),
+        }
         
-    # Buscamos la emoción dominante
-    dominant_emotion = max(scores, key=scores.get)
-    scores['dominant_emotion'] = dominant_emotion
+        # Buscamos la emoción dominante
+        dominant_emotion = max(scores, key=scores.get)
+        scores['dominant_emotion'] = dominant_emotion
         
-    return scores
+        return scores
+
+    elif response.status_code == 400:
+        # Retorna un diccionario con valor None
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     
--- server.py
+++ server.py
@@ -7,6 +7,9 @@ def emotion_detect():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
+
     formatted_response = (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, "
