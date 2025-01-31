fd4da1d485dca201076ee48e86717bfafd6fa5d4
Vegablues <eminyeni@gmail.com>
2025-01-31 10:04:53 -0500

    Initial Commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,12 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotion = formatted_response['emotionPredictions']['emotion']
+    emotion_score = formatted_response['emotionPredictions']['emotion_score']
+    return {'emotion': emoti, 'emotion_score': emotion_score}
