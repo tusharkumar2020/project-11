e6e9d95301ae3a0fcb1a074b5b92036a8e8958bf
soF0S <S0F0S@hotmail.com>
2025-02-05 15:18:08 -0500

    Modified emotion_detection.py file

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,7 @@
 # emotion_detection.py 
-’’’
-Task 2: Create an emotion detection application 
-              using the Watson NLP library.
-’’’
+# Task 2: Create an emotion detection application 
+#              using the Watson NLP library.
+
 import requests
 import json
 
@@ -13,7 +12,7 @@ HEADERS = {
     "Content-Type": "application/json"
 }
 
-# Here is my emotion_detector() function. I use the Watson NLP library.
+# Here is my emotion_detector function. I use the Watson NLP library.
 def emotion_detector(text_to_analyse):
     # JSON payload
     jsonPayload = {"raw_document": {"text": text_to_analyse}}
@@ -21,14 +20,24 @@ def emotion_detector(text_to_analyse):
     try:
         response = requests.post(URL, headers=HEADERS, json=jsonPayload)
 
+        # This is to Check ALL Raw Response
+        # print("Raw Response:", response.text)
+
         # response check
         if response.status_code == 200:
             response_data = response.json()
-            return response_data.get("text", "No text found in response")
+
+            #return response_data.get("text", "No text found in response")
+            # if there is emotion predictions, export the text
+            if "emotionPredictions" in response_data and response_data["emotionPredictions"]:
+                return response_data["emotionPredictions"][0]["emotionMentions"][0]["span"]["text"]
+
+            return "No emotion predictions found"
+
         else:
             return f"Error: {response.status_code}, {response.text}"
 
     except requests.exceptions.RequestException as e:
         return f"Request failed: {str(e)}"
-        
+
 
