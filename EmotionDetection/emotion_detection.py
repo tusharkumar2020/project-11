274239f26e1ff719bdd3729a8940988aa4d8a597
tony-the-coder <tonythecoder@outlook.com>
2025-02-15 18:44:16 -0500

    Initial commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = {"raw_document": {"text": text_to_analyze}}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=myobj, headers=header, timeout=10)
+
+    if response.status_code == 200:
+        try:
+            json_response = response.json()
+            emotions = json_response['emotionPredictions'][0]['emotionMentions'][0]['emotion']
+            dominant_emotion = max(emotions, key=emotions.get, default=None)
+            emotion = {
+                **emotions,
+                'dominant_emotion' : dominant_emotion
+            }
+            return emotion
+
+        except (KeyError, IndexError) as e:
+            print(f"Error extracting text from JSON: {e}")
+            print(json.dumps(json_response, indent=4)) # Print the full JSON for debugging
+            return "Error: Could not extract text"
+
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return f"Error: {response.status_code}"
