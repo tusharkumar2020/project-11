86c199a93dff07744f23f4f7a0e29186a30aaa92
KalavakolanuAnvesh <kalavakolanuanvesh@gmail.com>
2025-02-16 10:56:52 -0500

    finalcode

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,16 +1,20 @@
 import requests
+import json
 def emotion_detector(text_to_analyse):
     url= 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }  
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  
     response = requests.post(url, json = myobj, headers=header)  
-    return response.text
-
+    
     formatted_response = json.loads(response.text)
-    anger = formatted_response['emotionPredictions']['emotion']['anger']
-    disgust = formatted_response['emotionPredictions']['emotion']['disgust']
-    fear = formatted_response['emotionPredictions']['emotion']['fear']
-    joy = formatted_response['emotionPredictions']['emotion']['joy']
-    sadness = formatted_response['emotionPredictions']['emotion']['sadness']
-  
-    return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness}
+
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion = max(emotions, key=emotions.get) 
+
+    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness,'dominant_emotion': dominant_emotion}
