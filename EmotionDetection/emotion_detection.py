659d7f2985f94e65ccd71c079941ec00029bd56e
KalavakolanuAnvesh <kalavakolanuanvesh@gmail.com>
2025-02-19 06:48:32 -0500

    final submission

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,14 +7,24 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)  
     
     formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
+        dominant_emotion = max(emotions, key=emotions.get) 
 
-    emotions = formatted_response["emotionPredictions"][0]["emotion"]
-    dominant_emotion = max(emotions, key=emotions.get) 
+        anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
 
-    anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness,'dominant_emotion': dominant_emotion}
 
-    return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness,'dominant_emotion': dominant_emotion}
+    elif response.status_code == 400:
+        formatted_response ={
+                            'anger': None,
+                            'disgust': None, 
+                            'fear': None, 
+                            'joy': None, 
+                            'sadness': None, 
+                            'dominant_emotion': None}
+        return formatted_response
