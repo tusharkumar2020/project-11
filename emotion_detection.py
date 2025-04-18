ac383364a0f578ba47e27891cdbcef8ae9faba27
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-18 02:20:16 -0400

    completed task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,10 +4,28 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
     formatted_response = json.loads(response.text)
-    anger = formatted_response['emotionPredictions']['anger']
-    disgust = formatted_response['emotionPredictions']['disgust']
-    fear = formatted_response['emotionPredictions']['fear']
-    joy = formatted_response['emotionPredictions']['joy']
-    sadness = formatted_response['emotionPredictions']['sadness']
+    first_prediction = formatted_response['emotionPredictions'][0]
+    emotion_scores = first_prediction['emotion']
+    anger_score = emotion_scores['anger']
+    disgust_score = emotion_scores['disgust']
+    fear_score = emotion_scores['fear']
+    joy_score = emotion_scores['joy']
+    sadness_score = emotion_scores['sadness']
+    emotions = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(emotions, key=emotions.get)
+    output_format = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    return output_format
