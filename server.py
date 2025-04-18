e7cd5c47ba9066a9773e49b73c2fbe5d7a174869
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-18 03:28:46 -0400

    working on task 6

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 
+app = Flask(__name__)
+
 @app.route("/emotionDetector")
+def emotion_analyzer():
+     text_to_analyze = request.args.get('textToAnalyze')
+     emotion_result = emotion_detector(text_to_analyze)
+     return jsonify(emotion_result)
 
-
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
