c30f370fface467858f4f3aaba6e9bd0b9f8db5f
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-18 07:20:13 -0400

    working on task 7

--- server.py
+++ server.py
@@ -9,21 +9,22 @@ def emotion_analyzer():
     text_to_analyze = request.args.get('textToAnalyze')
     emotion_result = emotion_detector(text_to_analyze)
     
-    # return jsonify(emotion_result)
+    if emotion_result['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    else:
+        anger = emotion_result.get('anger', 0.0)
+        disgust = emotion_result.get('disgust', 0.0)
+        fear = emotion_result.get('fear', 0.0)
+        joy = emotion_result.get('joy', 0.0)
+        sadness = emotion_result.get('sadness', 0.0)
+        dominant_emotion = emotion_result.get('dominant_emotion', '')
 
-    anger = emotion_result.get('anger', 0.0)
-    disgust = emotion_result.get('disgust', 0.0)
-    fear = emotion_result.get('fear', 0.0)
-    joy = emotion_result.get('joy', 0.0)
-    sadness = emotion_result.get('sadness', 0.0)
-    dominant_emotion = emotion_result.get('dominant_emotion', '')
-
-    formatted_response = (
-        f"For the given statement, the system response is 'anger': {anger}, "
-        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
-        f"The dominant emotion is {dominant_emotion}."
-    )
-    return formatted_response
+        formatted_response = (
+            f"For the given statement, the system response is 'anger': {anger}, "
+            f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+            f"The dominant emotion is {dominant_emotion}."
+        )
+        return formatted_response
 
 @app.route("/")
 def render_index_page():
