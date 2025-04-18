78629521e997bd96e58f9b490f832cb48d497175
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-18 02:53:38 -0400

    working on task 5

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -6,4 +6,19 @@ class TestEmotionDetection(unittest.TestCase):
         def test_sentiment_analyzer(self):
         
         result_1 = emotion_detection('I am glad this happened')
-        self.assertEqual(result_1['label'], 'joy')
+        self.assertEqual(result_1['dominant_emotion'], 'joy')
+        
+        result_2 = emotion_detection('I am really mad about this')
+        self.assertEqual(result_1['dominant_emotion'], 'anger')
+        
+        result_3 = emotion_detection('I feel disgusted just hearing about this')
+        self.assertEqual(result_1['dominant_emotion'], 'disgust')
+        
+        result_4 = emotion_detection('I am so sad about this')
+        self.assertEqual(result_1['dominant_emotion'], 'sadness')
+        
+        result_5 = emotion_detection('I am really afraid that this will happen')
+        self.assertEqual(result_1['dominant_emotion'], 'fear')
+
+if __name__ == '__main__':
+    unittest.main()
