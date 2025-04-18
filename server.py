276d8e5ae775e17288b2aafb0b0f54c544df07c7
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-18 03:41:35 -0400

    completed task 6

--- server.py
+++ server.py
@@ -5,9 +5,25 @@ app = Flask(__name__)
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
-     text_to_analyze = request.args.get('textToAnalyze')
-     emotion_result = emotion_detector(text_to_analyze)
-     return jsonify(emotion_result)
+
+    text_to_analyze = request.args.get('textToAnalyze')
+    emotion_result = emotion_detector(text_to_analyze)
+    
+    # return jsonify(emotion_result)
+
+    anger = emotion_result.get('anger', 0.0)
+    disgust = emotion_result.get('disgust', 0.0)
+    fear = emotion_result.get('fear', 0.0)
+    joy = emotion_result.get('joy', 0.0)
+    sadness = emotion_result.get('sadness', 0.0)
+    dominant_emotion = emotion_result.get('dominant_emotion', '')
+
+    formatted_response = (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+    return formatted_response
 
 @app.route("/")
 def render_index_page():
