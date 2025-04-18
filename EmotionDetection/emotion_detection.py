5224a3cc375cc3e59bff1b332f97279b1bcae19d
Tushar Kumar <tusharkumar@hotmail.com>
2025-04-18 07:13:40 -0400

    working on task 7

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,28 +4,48 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
-    formatted_response = json.loads(response.text)
-    first_prediction = formatted_response['emotionPredictions'][0]
-    emotion_scores = first_prediction['emotion']
-    anger_score = emotion_scores['anger']
-    disgust_score = emotion_scores['disgust']
-    fear_score = emotion_scores['fear']
-    joy_score = emotion_scores['joy']
-    sadness_score = emotion_scores['sadness']
-    emotions = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score
-    }
-    dominant_emotion = max(emotions, key=emotions.get)
-    output_format = {
-        'anger': anger_score,
-        'disgust': disgust_score,
-        'fear': fear_score,
-        'joy': joy_score,
-        'sadness': sadness_score,
-        'dominant_emotion': dominant_emotion
-    }
-    return output_format
+    
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    elif response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        first_prediction = formatted_response['emotionPredictions'][0]
+        emotion_scores = first_prediction['emotion']
+    
+        anger_score = emotion_scores['anger']
+        disgust_score = emotion_scores['disgust']
+        fear_score = emotion_scores['fear']
+        joy_score = emotion_scores['joy']
+        sadness_score = emotion_scores['sadness']
+    
+        emotions = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score
+        }
+    
+        dominant_emotion = max(emotions, key=emotions.get)
+    
+        output_format = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion
+        }
+    
+        return output_format
+    else:
+        return {
+            'error': f"Request failed with status code {response.status_code}"
+        }
